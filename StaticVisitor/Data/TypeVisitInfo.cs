a39c8639f6f1f890995b0f30323b44bff8cb72c0
Michele Di Cosmo <michele@helixcentre.com>
2019-11-07 10:59:27 +0000

    docstring

--- StaticVisitor/Data/TypeVisitInfo.cs
+++ StaticVisitor/Data/TypeVisitInfo.cs
@@ -2,6 +2,9 @@
 
 namespace Sid.Tools.StaticVisitor
 {
+    /// <summary>
+    /// Represents the visit to a type
+    /// </summary>
     public abstract class TypeVisit
     {
         protected TypeVisit(Type type)
@@ -17,54 +20,79 @@ namespace Sid.Tools.StaticVisitor
         }
     }
     
+    /// <summary>
+    /// Represents the visit to the type passed to the visitor
+    /// </summary>
     public class InitialTypeTypeVisit : TypeVisit
     {
         public InitialTypeTypeVisit(Type initialType): base(initialType) {}
     }
 
+    /// <summary>
+    /// Represents the visit to a type caused by the previous type inheriting it
+    /// </summary>
     public abstract class InheritingTypeTypeVisit : TypeVisit
     {
         protected InheritingTypeTypeVisit(Type inheritingType): base(inheritingType) {}
     }
 
+    /// <summary>
+    /// Represents the visit to a type caused by it being the base type of the previous type
+    /// </summary>
     public class InheritingBaseTypeTypeVisit : InheritingTypeTypeVisit
     {
         public InheritingBaseTypeTypeVisit(Type baseType): base(baseType){}
     }
-
+    
+    /// <summary>
+    /// Represents the visit to a type (an interface) caused by the previous type implementing it
+    /// </summary>
     public class InheritingInterfaceTypeVisit : InheritingTypeTypeVisit
     {
         public InheritingInterfaceTypeVisit(Type @interface): base(@interface){}
     }
 
+    /// <summary>
+    /// Represents the visit to a type caused by the previous type encompassing it
+    /// </summary>
     public abstract class EncompassedTypeTypeVisit : TypeVisit
     {
         protected EncompassedTypeTypeVisit(Type encompassedType): base(encompassedType) {}
     }
     
+    /// <summary>
+    /// Represents the visit to a type cause by the previous type being a generic type and specifying it as a parameter type
+    /// </summary>
     public class ParameterTypeTypeVisit : EncompassedTypeTypeVisit
     {
         public ParameterTypeTypeVisit(Type parameterType): base(parameterType) {}
     }
-
+    
+    /// <summary>
+    /// Represents the visit to a type cause by the previous type having it as an element type (e.g. being an array of this type)
+    /// </summary>
     public class ElementTypeTypeVisit : EncompassedTypeTypeVisit
     {
         public ElementTypeTypeVisit(Type elementType): base(elementType) {}
     }
-
+    
+    /// <summary>
+    /// Represents the visit to a type cause by it being a derived type of (assignable type to) the previous type
+    /// </summary>
     public class AssignableTypeTypeVisit : TypeVisit
     {
         public AssignableTypeTypeVisit(Type assignableType): base(assignableType) {}
     }
     
+    /// <summary>
+    /// Represents the visit to a type cause by the previous type having a property of this type
+    /// </summary>
     public class PropertyTypeVisit : TypeVisit
     {
         public PropertyTypeVisit(Type propertyType, string propertyName):base(propertyType)
         {
             PropertyName = propertyName;
         }
-
-        public Type PropertyType => @Type;
 
         public string PropertyName { get; }
     }
